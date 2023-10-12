86e0ed96db0244ced58ca17494173ca2ea959580
bharathiraja-developer <bharathiraja798@gmail.com>
2023-10-12 21:40:06 +0530

    passing props

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+function Hello(props) {
+  return <h1>Hello {props.name}</h1>;
+}
+
 function App() {
-  return <p>Hello World!</p>;
+  let name1 = "Bharathiraja";
+  return (
+    <div>
+      <Hello name={name1} />
+      <p>Hello World!</p>
+    </div>
+  );
 }
 
 export default App;
