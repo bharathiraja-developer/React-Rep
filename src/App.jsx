187ab5e09d0c043d9ca2f2f2a7860b246a3f6905
bharathiraja-developer <bharathiraja798@gmail.com>
2023-10-17 08:02:38 +0530

    Class task

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
-function Hello(props) {
-  return <h1>Hello {props.name}</h1>;
-}
+import React, { Component } from "react";
 
-function App() {
-  let name1 = "Bharathi Raja";
-  return (
-    <div>
-      <Hello name={name1} />
-      <p>Hello World!</p>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+  }
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  };
+  reset = () => {
+    this.setState({
+      count: 0,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <p>count : {this.state.count}</p>
+        <button onClick={this.increment}>increment</button>
+        <button onClick={this.decrement}>decrement</button>
+        <button onClick={this.reset}>reset</button>
+      </div>
+    );
+  }
 }
 
 export default App;
