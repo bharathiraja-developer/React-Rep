90f9eaf39ed7709f106eda1475fa234673958d9c
bharathiraja-developer <bharathiraja798@gmail.com>
2023-10-18 22:44:37 +0530

    day4-class task

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,34 @@
-import React, { useState } from "react";
-import Display from "./Components/Display";
-
-function Button(props) {
-  return <button onClick={props.onClick}>{props.ButtonName}</button>;
-}
+import React from "react";
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import Notes from "./Components/Notes";
+import Users from "./Components/Users";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function handlePlusClick() {
-    setCounter(counter + 1);
-  }
-
-  function handleMinusClick() {
-    setCounter(counter - 1);
-  }
-
-  function handleZeroClick() {
-    setCounter(0);
-  }
-
+  const padding = {
+    padding: 5,
+  };
   return (
-    <div>
-      <Display counter={counter} />
-      <Button onClick={handlePlusClick} ButtonName="plus" />
-      <Button onClick={handleMinusClick} ButtonName="minus" />
-      <Button onClick={handleZeroClick} ButtonName="zero" />
-    </div>
+    <Router>
+      <div>
+        <Link to="/" style={padding}>
+          Home
+        </Link>
+        <Link to="/notes" style={padding}>
+          Notes
+        </Link>
+        <Link to="/users" style={padding}>
+          users
+        </Link>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/notes" element={<Notes />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
-/*
-
-Button tag like this :
-
-<Button onClick={handlePlusClick} ButtonName="plus" />
-<Button onClick={handleMinusClick} ButtonName="minus" />
-<Button onClick={handleZeroClick} ButtonName="zero" />
-
-Button.jsx file return funtion :
-
-function Button(props) {
-    return <button onClick={props.onClick}>{props.ButtonName}</button>;
-  }
-*/
--- src/Components/Display.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Display({ counter }) {
-  return <p>{counter}</p>;
-}
-
-export default Display;
